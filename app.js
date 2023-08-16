ac7b67bec2c237f6201b3a9f3b35e6d00c0df32e
iamatifmoin <atifmoin3@gmail.com>
2023-08-16 22:00:35 +0530

    added projects

--- app.js
+++ app.js
@@ -20,12 +20,6 @@ const printProjects = () => {
                 </svg>
                 GitHub
               </a>
-              <a href="${item.live}" target="_blank">
-                <svg width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24">
-                  <path fill="currentColor" d="M14 3v2h3.59l-9.83 9.83l1.41 1.41L19 6.41V10h2V3m-2 16H5V5h7V3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7h-2v7Z"/>
-                </svg>
-                Live site
-              </a>
             </div>
         </article>
     `;
--- projects.js
+++ projects.js
@@ -1,27 +1,22 @@
 export default [
   {
-    title: "Metal3",
+    title: "Hand Gesture Recognition",
+    description: "A software to recognise hand gestures, built using python.",
+    image: "",
+    github: "https://github.com/shikharcookies/hand-gesture-recognition",
+  },
+  {
+    title: "Spotify API",
     description:
-      "A mock website for Metal3.io built using JavaScript, CSS & HTML.",
-    image:
-      "https://raw.githubusercontent.com/iamatifmoin/metal3/main/assets/Desktop1.png",
-    github: "https://github.com/iamatifmoin/metal3",
-    live: "https://metal3-iamatifmoin.netlify.app/",
+      "A software that fetches data from the Spotify API, built using Python.",
+    image: "",
+    github: "https://github.com/shikharcookies/spotify-api",
   },
   {
-    title: "BREATHE",
-    description: "A web-app that lets you check the Air Quality in your city.",
-    image:
-      "https://raw.githubusercontent.com/iamatifmoin/BREATHE/master/assets/BREATHE.png",
-    github: "https://github.com/iamatifmoin/BREATHE",
-    live: "https://breathe-iamatifmoin.netlify.app/",
-  },
-  {
-    title: "Portfolio",
-    description: "A portflio website built using JavaScript, CSS & HTML.",
-    image:
-      "https://raw.githubusercontent.com/iamatifmoin/portfolio-website/master/assets/portfolio.png",
-    github: "https://github.com/iamatifmoin/portfolio",
-    live: "https://iamatifmoin.github.io/portfolio/",
+    title: "Recipe API",
+    description:
+      "A software that fetches data from a recipe API, built using Python.",
+    image: "",
+    github: "https://github.com/shikharcookies/recipe-api",
   },
 ];
