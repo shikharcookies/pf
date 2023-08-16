af55f4a52f315ff4ed0bc9ac17a0fe0b9363eddc
iamatifmoin <atifmoin3@gmail.com>
2023-08-16 17:58:10 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<div align="left"> <h1>Portfolio</h1>
+<p>A personal portfolio built using JavaScript, CSS & HTML.</p></div>
+
+## Screenshots
+
+![portfolio](./assets/portfolio.png)
---
+++ projects.js
@@ -0,0 +1,27 @@
+export default [
+  {
+    title: "Metal3",
+    description:
+      "A mock website for Metal3.io built using JavaScript, CSS & HTML.",
+    image:
+      "https://raw.githubusercontent.com/iamatifmoin/metal3/main/assets/Desktop1.png",
+    github: "https://github.com/iamatifmoin/metal3",
+    live: "https://metal3-iamatifmoin.netlify.app/",
+  },
+  {
+    title: "BREATHE",
+    description: "A web-app that lets you check the Air Quality in your city.",
+    image:
+      "https://raw.githubusercontent.com/iamatifmoin/BREATHE/master/assets/BREATHE.png",
+    github: "https://github.com/iamatifmoin/BREATHE",
+    live: "https://breathe-iamatifmoin.netlify.app/",
+  },
+  {
+    title: "Portfolio",
+    description: "A portflio website built using JavaScript, CSS & HTML.",
+    image:
+      "https://raw.githubusercontent.com/iamatifmoin/portfolio-website/master/assets/portfolio.png",
+    github: "https://github.com/iamatifmoin/portfolio",
+    live: "https://iamatifmoin.github.io/portfolio/",
+  },
+];
