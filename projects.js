736d2a11c1b36154fe59b34b7073132536d2f0d5
iamatifmoin <atifmoin3@gmail.com>
2023-08-17 19:19:39 +0530

    updated images

--- projects.js
+++ projects.js
@@ -2,21 +2,21 @@ export default [
   {
     title: "Hand Gesture Recognition",
     description: "A software to recognise hand gestures, built using python.",
-    image: "",
+    image: "./assets/handimg.jpeg",
     github: "https://github.com/shikharcookies/hand-gesture-recognition",
   },
   {
     title: "Spotify API",
     description:
       "A software that fetches data from the Spotify API, built using Python.",
-    image: "",
+    image: "./assets/spotifyimg.jpeg",
     github: "https://github.com/shikharcookies/spotify-api",
   },
   {
     title: "Recipe API",
     description:
       "A software that fetches data from a recipe API, built using Python.",
-    image: "",
+    image: "./assets/recipeimg.jpeg",
     github: "https://github.com/shikharcookies/recipe-api",
   },
 ];
